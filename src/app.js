2ba0bfed2be5e95c5b969963de39d36f1a513179
pulak-sarmah <pulaksarmah333@gmail.com>
2024-02-27 13:18:41 +0530

    Refactor app.js: Reorder imports, add rate limiting, and integrate Swagger UI

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
-import Express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
+import cors from "cors";
+import Express from "express";
+import rateLimit from "express-rate-limit";
+import swaggerUi from "swagger-ui-express";
 import errorHandler from "./middlewares/errorHandler.js";
+import swaggerSpec from "./utils/swagger.js";
+
 const app = Express();
 
 const clientUrls = process.env.CLIENT_URL.split(",");
@@ -21,22 +25,32 @@ app.use(Express.static("public"));
 
 app.use(cookieParser());
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 200,
+});
+
+app.use(limiter);
+
+//swagger route
+app.use("/swaggerUI", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
 //route imports
-import userRouter from "./routes/user.routes.js";
-import videoRouter from "./routes/videos.routes.js";
-import playlistRouter from "./routes/playlist.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import playlistRouter from "./routes/playlist.routes.js";
 import subscribtionRouter from "./routes/subscribtion.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
+import userRouter from "./routes/user.routes.js";
+import videoRouter from "./routes/videos.routes.js";
 
 // routes declearation
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/playlists", playlistRouter);
 app.use("/api/v1/comments", commentRouter);
-app.use("/api/v1/like", likeRouter);
-app.use("/api/v1/subscribtion", subscribtionRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/subscriptions", subscribtionRouter);
 app.use("/api/v1/tweets", tweetRouter);
 
 app.use(errorHandler);
